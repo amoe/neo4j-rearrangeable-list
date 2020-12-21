737cf91ea4fa90afb22db6af02e34abb7fe936bb
David Banks <amoebae@gmail.com>
2020-12-21 11:20:32 +0000

    move state

--- neo4j_rearrangeable_list.py
+++ neo4j_rearrangeable_list.py
@@ -10,16 +10,43 @@ r.position AS position
 ORDER BY r.position
 """
 
+MOVE_QUERY = """
+MATCH (l:List {id: {list_id}}),
+      (i1:Item {id: {source_item_id}})-[r1:IN_LIST]->(l),
+      (i2:Item {id: {target_item_id}})-[r2:IN_LIST]->(l)
+WITH
+    r1.position AS oldPosition,
+    r2.position AS newPosition,
+    r1 AS r1,
+    l AS l,
+    i1 AS i1,
+    CASE
+      WHEN r2.position < r1.position THEN 1
+      WHEN r2.position > r1.position THEN -1
+      ELSE 0
+    END AS signum
+ MATCH (:Item)-[r3:IN_LIST]->(l)
+WHERE
+   (newPosition < oldPosition
+   AND r3.position >= newPosition AND r3.position < oldPosition)
+   OR (newPosition > oldPosition
+   AND r3.position > oldPosition AND r3.position <= newPosition)
+SET r3.position = r3.position + signum, r1.position = newPosition
+RETURN i1
+"""
+
 class ListManager:
     def __init__(self, session):
         self.session = session
 
     def move(self, list_id, source_item_id, target_item_id):
-        pass
-
+        r = self.session.run(MOVE_QUERY, {
+            'list_id': list_id,
+            'source_item_id': source_item_id,
+            'target_item_id': target_item_id
+        })
+        return r.data()
 
     def retrieve_list(self, list_id):
         r = self.session.run(QRY, {'list_id': list_id})
         return r.data()
-
-    
--- test/test_operations.py
+++ test/test_operations.py
@@ -7,8 +7,21 @@ INITIAL_EXPECTED_STATE = [
     {'description': 'Leela', 'id': 'c', 'position': 2}
 ]
 
+
+STATE_AFTER_MOVE = [
+    {'description': 'Bender', 'id': 'b', 'position': 0},
+    {'description': 'Leela', 'id': 'c', 'position': 1},
+    {'description': 'Fry', 'id': 'a', 'position': 2},
+]
+
+
 def test_sanity(session):
     manager = ListManager(session)
     r = manager.retrieve_list(42)
     assert r == INITIAL_EXPECTED_STATE
-    
+
+
+def test_move(session):
+    manager = ListManager(session)
+    manager.move(42, 'a', 'c')   
+    assert manager.retrieve_list(42) == STATE_AFTER_MOVE
